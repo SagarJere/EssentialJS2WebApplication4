2fdc47006b026a1db6f3f997fc19b39bed05e8b7
sagar <sagarjere@hotmail.com>
2019-08-24 16:22:58 +0530

    Add project files.

---
+++ EssentialJS2WebApplication4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EssentialJS2WebApplication4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
